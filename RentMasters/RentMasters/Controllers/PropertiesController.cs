ecc6f574f511c23e19bfacd8cfd5ce8045b21d02
Amadeusz <amad.snar@gmail.com>
2018-03-01 20:46:10 +0100

    migracje + API(sam GET)

---
+++ RentMasters/RentMasters/Controllers/PropertiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentMasters.Models;
+using RentMasters.Models.Database;
+
+namespace RentMasters.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Properties")]
+    public class PropertiesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public PropertiesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Properties
+        [HttpGet]
+        [Route("get")]
+        public IEnumerable<Property> GetProperties()
+        {
+            return _context.Properties;
+        }
+
+        // GET: api/Properties/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProperty([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @property = await _context.Properties.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(@property);
+        }
+
+        // PUT: api/Properties/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProperty([FromRoute] int id, [FromBody] Property @property)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != @property.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@property).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Properties
+        [HttpPost]
+        public async Task<IActionResult> PostProperty([FromBody] Property @property)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Properties.Add(@property);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProperty", new { id = @property.Id }, @property);
+        }
+
+        // DELETE: api/Properties/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProperty([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var @property = await _context.Properties.SingleOrDefaultAsync(m => m.Id == id);
+            if (@property == null)
+            {
+                return NotFound();
+            }
+
+            _context.Properties.Remove(@property);
+            await _context.SaveChangesAsync();
+
+            return Ok(@property);
+        }
+
+        private bool PropertyExists(int id)
+        {
+            return _context.Properties.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RentMasters/RentMasters/Migrations/20180301184847_SeedingDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RentMasters.Migrations
+{
+    public partial class SeedingDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Owners (Name, Surname, Phone) VALUES ('Amadeusz', 'Snarski', '888999777')");
+            migrationBuilder.Sql("INSERT INTO Owners (Name, Surname, Phone) VALUES ('Arkadiusz', 'Michalski', '999666333')");
+            migrationBuilder.Sql("INSERT INTO Owners (Name, Surname, Phone) VALUES ('Mateusz', 'Matela', '777444111')");
+
+            migrationBuilder.Sql("INSERT INTO Addresses (Street, City) VALUES ('Wiadukt', 'Białystok')");
+            migrationBuilder.Sql("INSERT INTO Addresses (Street, City) VALUES ('Bema', 'Białystok')");
+            migrationBuilder.Sql("INSERT INTO Addresses (Street, City) VALUES ('Pogodna', 'Białystok')");
+
+            migrationBuilder.Sql("INSERT INTO Properties (Type, Description, Rooms, Area, Washer, Refrigerator, Iron, AddressId, OwnerId) VALUES (0, 'Opis1', 7, 170, 1, 1, 1, 1, 1 )");
+            migrationBuilder.Sql("INSERT INTO Properties (Type, Description, Rooms, Area, Washer, Refrigerator, Iron, AddressId, OwnerId) VALUES (1, 'Opis2', 4, 50, 1, 0, 1, 2, 2 )");
+            migrationBuilder.Sql("INSERT INTO Properties (Type, Description, Rooms, Area, Washer, Refrigerator, Iron, AddressId, OwnerId) VALUES (1, 'Opis3', 2, 29, 0, 0, 1, 3, 3 )");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Owners");
+            migrationBuilder.Sql("DELETE FROM Addresses");
+            migrationBuilder.Sql("DELETE FROM Properties");
+        }
+    }
+}
